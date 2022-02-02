3f4df2244972bf47ce29cf9d4bcfc2ecfb7b2614
francescocastelli <francesco8.castelli@mail.polimi.it>
2022-02-02 12:10:47 +0100

    update tests

--- test/test.py
+++ test/test.py
@@ -1,18 +1,98 @@
 import torch
 import unittest
+from torch.nn.utils.rnn import pad_sequence
+from parameterized import parameterized
 from editdistance import editdistance
+from utils import Tokenizer, padToken
 
 class EditDistanceTest(unittest.TestCase):
-    def test_cer(self):
+    def setUp(self):
+        self.tokenizer = Tokenizer()
 
+    @parameterized.expand([
+        ['cpu'],
+        ['cuda:0']
+    ])
+    def test_no_batch_ins(self, device):
         # ref: [TEST]
         # hyp: [TESST]
 
-        x = torch.tensor([1, 2, 3, 1], dtype=torch.int)
-        y = torch.tensor([1, 2, 3, 3, 1], dtype=torch.int)
+        x = self.tokenizer.tokenize('test', device)
+        y = self.tokenizer.tokenize('tesst', device)
 
-        r = editdistance(x, y)
-        self.assertEqual(r, 1)
+        r = editdistance(x, y, padToken)
+        self.assertEqual(r.item(), 1)
+
+    @parameterized.expand([
+        ['cpu'],
+        ['cuda:0']
+    ])
+    def test_no_batch_del(self, device):
+        # ref: [TEST]
+        # hyp: [TST]
+
+        x = self.tokenizer.tokenize('test', device)
+        y = self.tokenizer.tokenize('tst', device)
+
+        r = editdistance(x, y, padToken)
+        self.assertEqual(r.item(), 1)
+
+    @parameterized.expand([
+        ['cpu'],
+        ['cuda:0']
+    ])
+    def test_no_batch_sub(self, device):
+        # ref: [TEST]
+        # hyp: [TAST]
+
+        x = self.tokenizer.tokenize('test', device)
+        y = self.tokenizer.tokenize('tast', device)
+
+        r = editdistance(x, y, padToken)
+        self.assertEqual(r.item(), 1)
+
+    @parameterized.expand([
+        ['cpu'],
+        ['cuda:0']
+    ])
+    def test_batch_ins(self, device):
+        # ref: [[TEST], [HELLO]]
+        # hyp: [[TESTT], [HELLOO]]
+
+        ref = [self.tokenizer.tokenize('test', device), self.tokenizer.tokenize('hello', device)]
+        hyp = [self.tokenizer.tokenize('testt', device), self.tokenizer.tokenize('helloo', device)] 
+        res = [1, 1]
+
+        # padding
+        x = pad_sequence(ref, batch_first=True, padding_value=padToken)
+        y = pad_sequence(hyp, batch_first=True, padding_value=padToken)
+
+        pred = editdistance(x, y, padToken).to('cpu')
+
+        for r, p in zip(res, pred): 
+            self.assertEqual(r, p.item())
+
+    @parameterized.expand([
+        ['cpu'],
+        ['cuda:0']
+    ])
+    def test_batch_del(self, device):
+        # ref: [[TEST], [HELLO]]
+        # hyp: [[TET], [HELO]]
+
+        ref = [self.tokenizer.tokenize('test', device), self.tokenizer.tokenize('hello', device)]
+        hyp = [self.tokenizer.tokenize('tet', device), self.tokenizer.tokenize('helo', device)] 
+        res = [1, 1]
+
+        # padding
+        x = pad_sequence(ref, batch_first=True, padding_value=padToken)
+        y = pad_sequence(hyp, batch_first=True, padding_value=padToken)
+
+        pred = editdistance(x, y, padToken).to('cpu')
+
+        for r, p in zip(res, pred): 
+            self.assertEqual(r, p.item())
+
 
 if __name__ == "__main__":
     unittest.main()
